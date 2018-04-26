bb1158806dec71ba33851008ff92771181753e12
Benedetto Lo Giudice <bennythejudge@users.noreply.github.com>
2018-04-27 00:32:07 +0200

    Solve problem

---
+++ current_age_for_birth_year.rb
@@ -0,0 +1,5 @@
+def current_age_for_birth_year(year)
+	this_year = Time.new.year
+	puts this_year - year
+	return this_year - year
+end
--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -4,6 +4,14 @@ describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
 
-    expect(age_of_person).to eq(19)
+    expect(age_of_person).to eq(Time.new.year - 1984)
+  end
+end
+
+describe "current_age_for_birth_year 2015 method" do
+  it "returns the age of a person born in 2015 as 2" do
+    age_of_person = current_age_for_birth_year(2015)
+
+    expect(age_of_person).to eq(Time.new.year - 2015)
   end
 end
